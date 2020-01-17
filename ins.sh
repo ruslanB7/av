f520fdd9b91e95b3f7ad9fe510c0865316dfcc76
Ruslan <mstrbsc@gmail.com>
2020-01-17 13:32:19 +0200

    21

--- ins.sh
+++ ins.sh
@@ -3,3 +3,5 @@
 sudo apt install avr-gcc avr-binutils avr-libc avrdude
 
 echo "https://sourceforge.net/projects/avr-eclipse/files/avr-eclipse%20beta%20versions/"
+
+https://www.microchip.com/mplab/avr-support/avr-and-arm-toolchains-c-compilers
